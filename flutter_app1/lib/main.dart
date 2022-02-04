0578232b12879a5ebc3ae3bed51ba2202ca67a74
Rasyid-deve <renaldirasyed@gmail.com>
2022-02-04 15:53:37 +0700

    Update Container Colors BG

--- flutter_app1/lib/main.dart
+++ flutter_app1/lib/main.dart
@@ -13,10 +13,14 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(title: Text("Contoh Alignment")),
         body: Container(
-          alignment: Alignment.bottomCenter,
+          margin: EdgeInsets.all(50),
+          height: 200,
+          width: 200,
+          alignment: Alignment.center,
+          color: Colors.amber[900],
           child: Text(
             'Semangat Belajar',
-            style: TextStyle(fontSize: 20),
+            style: TextStyle(fontSize: 20, color: Colors.white),
           ),
         ),
       ),
