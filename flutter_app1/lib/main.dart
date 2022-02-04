1e72744c98bd51137a91b1b6a796cc9c9ba72662
Rasyid-deve <renaldirasyed@gmail.com>
2022-02-04 15:58:29 +0700

    Update Container Margin

--- flutter_app1/lib/main.dart
+++ flutter_app1/lib/main.dart
@@ -11,15 +11,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("Contoh Height and Width")),
+        appBar: AppBar(title: Text("Contoh Margin")),
         body: Container(
           margin: EdgeInsets.all(50),
-          height: 200,
-          width: 200,
-          alignment: Alignment.center,
+          height: 250,
+          width: 250,
+          alignment: Alignment.topLeft,
           color: Colors.blueGrey,
           child: Text(
-            'Semangat Belajar',
+            'Semangat Belajar Flutter',
             style: TextStyle(fontSize: 20, color: Colors.white),
           ),
         ),
