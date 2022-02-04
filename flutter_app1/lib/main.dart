fd856b58eb40d6578788693f56f4779227656c76
Rasyid-deve <renaldirasyed@gmail.com>
2022-02-04 16:20:10 +0700

    Update Container Transform

--- flutter_app1/lib/main.dart
+++ flutter_app1/lib/main.dart
@@ -11,18 +11,24 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("Contoh Padding")),
+        appBar: AppBar(title: Text("Contoh Transform")),
         body: Container(
-          padding:EdgeInsets.only(left:20),
-          margin: EdgeInsets.all(20),
-          height: 250,
-          width: 250,
-          alignment: Alignment.topLeft,
-          color: Colors.blueGrey,
-          child: Text(
-            'Ayo Belajar Flutter',
-            style: TextStyle(fontSize: 20, color: Colors.white),
+          decoration: BoxDecoration(
+            image: const DecorationImage(
+              image: NetworkImage(
+                  'https://pixnio.com/free-images/2021/11/18/2021-11-18-08-43-31-850x567.jpg'
+              ),
+              fit: BoxFit.fitWidth,
+            ),
+            border: Border.all(
+              color: Colors.black,
+              width: 8,
+            ),
           ),
+          height: 200,
+          width: 300,
+          margin: const EdgeInsets.only(left: 30.0, right: 30.0, top: 30),
+          transform: Matrix4.rotationZ(-0.1),
         ),
       ),
     );
