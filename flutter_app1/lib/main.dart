9ab150f5643f2157cfe8278fac1c950b54352617
Rasyid-deve <renaldirasyed@gmail.com>
2022-02-05 18:53:35 +0700

    Update Column Widget

--- flutter_app1/lib/main.dart
+++ flutter_app1/lib/main.dart
@@ -11,25 +11,28 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("Contoh Configuration")),
-        body: Container(
-          decoration: BoxDecoration(
-            color: const Color(0xff7c94b6),
-            image: const DecorationImage(
-              image: NetworkImage(
-                  'https://pixnio.com/free-images/2021/11/18/2021-11-18-08-43-31-850x567.jpg'
+        appBar: AppBar(title: Text("Contoh Column Widget")),
+        body: Column(
+          children: [
+            Container (
+              color: Colors.purpleAccent,
+              child: const FlutterLogo(
+                size: 90.0,
+                ),
+            ),
+            Container(
+              color: Colors.greenAccent,
+              child: const FlutterLogo(
+                size: 90.0,
               ),
-              fit: BoxFit.fitWidth,
             ),
-            border: Border.all(
-              color: Colors.black,
-              width: 8,
+            Container(
+              color: Colors.orangeAccent,
+              child: const FlutterLogo(
+                size: 90.0,
+              ),
             ),
-            borderRadius: BorderRadius.circular(12),
-          ),
-          height: 200,
-          width: 300,
-          margin: EdgeInsets.all(20)
+          ],
         ),
       ),
     );
