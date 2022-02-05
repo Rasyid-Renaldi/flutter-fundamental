84ebb85d1b065760e77fdada5ea07aef404d7ea1
Rasyid-deve <renaldirasyed@gmail.com>
2022-02-05 18:47:08 +0700

    Update Container Configuration

--- flutter_app1/lib/main.dart
+++ flutter_app1/lib/main.dart
@@ -11,9 +11,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("Contoh Transform")),
+        appBar: AppBar(title: Text("Contoh Configuration")),
         body: Container(
           decoration: BoxDecoration(
+            color: const Color(0xff7c94b6),
             image: const DecorationImage(
               image: NetworkImage(
                   'https://pixnio.com/free-images/2021/11/18/2021-11-18-08-43-31-850x567.jpg'
@@ -24,11 +25,11 @@ class MyApp extends StatelessWidget {
               color: Colors.black,
               width: 8,
             ),
+            borderRadius: BorderRadius.circular(12),
           ),
           height: 200,
           width: 300,
-          margin: const EdgeInsets.only(left: 30.0, right: 30.0, top: 30),
-          transform: Matrix4.rotationZ(-0.1),
+          margin: EdgeInsets.all(20)
         ),
       ),
     );
