2253eba66516a7db25f76328aa85abf6866615d5
Rasyid-deve <renaldirasyed@gmail.com>
2022-02-05 19:05:38 +0700

    Update Stack

--- flutter_app1/lib/main.dart
+++ flutter_app1/lib/main.dart
@@ -10,27 +10,35 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(title: const Text("Contoh Row Widget")),
-        body: Row(
-          children: [
-            Container (
-              color: Colors.purpleAccent,
-              child: const FlutterLogo(
-                size: 90.0,
-                ),
+        appBar: AppBar(title: const Text("Contoh Stack Widget"),
+        ),
+        body: Stack(
+          children: <Widget>[
+            Container(
+              color: Colors.green,
+              alignment: Alignment.bottomCenter,
+              child: const Text("Satu", 
+              style: TextStyle(
+                  fontSize: 30, 
+                  color: Colors.white
+                  ),
+                  ),
             ),
             Container(
-              color: Colors.greenAccent,
-              child: const FlutterLogo(
-                size: 90.0,
-              ),
+              color: Colors.red,
+              alignment: Alignment.bottomCenter,
+              child: const Text("Dua", style: TextStyle(fontSize: 30, color: Colors.white)),
+              height: 400.0,
+              width: 300.0,
             ),
             Container(
-              color: Colors.orangeAccent,
-              child: const FlutterLogo(
-                size: 90.0,
-              ),
+              color: Colors.deepPurple,
+              alignment: Alignment.bottomCenter,
+              child: const Text("Tiga", style: TextStyle(fontSize: 30, color: Colors.white)),
+              height: 200.0,
+              width: 200.0,
             ),
           ],
         ),
